a1da1cb1fb9d8fd60eedf02182d2f23057fac743
mgimpel <mgimpel@677e43ea-1318-0410-b7e8-8a0e8f74a223>
2004-02-03 08:01:35 +0000

    Added Javadoc

--- main/trunk/src/org/xiph/speex/spi/SpeexEncoding.java
+++ main/trunk/src/org/xiph/speex/spi/SpeexEncoding.java
@@ -47,30 +47,74 @@ import javax.sound.sampled.AudioFormat;
 public class SpeexEncoding
   extends AudioFormat.Encoding
 {
-  /** Specifies any Speex encoded data. */
+  /** Specifies any Speex encoding. */
   public static final SpeexEncoding SPEEX = new SpeexEncoding("SPEEX");
-  public static final SpeexEncoding SPEEX_Q0 = new SpeexEncoding("SPEEX_quality_0", 0, false);
-  public static final SpeexEncoding SPEEX_Q1 = new SpeexEncoding("SPEEX_quality_1", 1, false);
-  public static final SpeexEncoding SPEEX_Q2 = new SpeexEncoding("SPEEX_quality_2", 2, false);
-  public static final SpeexEncoding SPEEX_Q3 = new SpeexEncoding("SPEEX_quality_3", 3, false);
-  public static final SpeexEncoding SPEEX_Q4 = new SpeexEncoding("SPEEX_quality_4", 4, false);
-  public static final SpeexEncoding SPEEX_Q5 = new SpeexEncoding("SPEEX_quality_5", 5, false);
-  public static final SpeexEncoding SPEEX_Q6 = new SpeexEncoding("SPEEX_quality_6", 6, false);
-  public static final SpeexEncoding SPEEX_Q7 = new SpeexEncoding("SPEEX_quality_7", 7, false);
-  public static final SpeexEncoding SPEEX_Q8 = new SpeexEncoding("SPEEX_quality_8", 8, false);
-  public static final SpeexEncoding SPEEX_Q9 = new SpeexEncoding("SPEEX_quality_9", 9, false);
-  public static final SpeexEncoding SPEEX_Q10 = new SpeexEncoding("SPEEX_quality_10", 10, false);
-  public static final SpeexEncoding SPEEX_VBR0 = new SpeexEncoding("SPEEX_VBR_quality_0", 0, true);
-  public static final SpeexEncoding SPEEX_VBR1 = new SpeexEncoding("SPEEX_VBR_quality_1", 1, true);
-  public static final SpeexEncoding SPEEX_VBR2 = new SpeexEncoding("SPEEX_VBR_quality_2", 2, true);
-  public static final SpeexEncoding SPEEX_VBR3 = new SpeexEncoding("SPEEX_VBR_quality_3", 3, true);
-  public static final SpeexEncoding SPEEX_VBR4 = new SpeexEncoding("SPEEX_VBR_quality_4", 4, true);
-  public static final SpeexEncoding SPEEX_VBR5 = new SpeexEncoding("SPEEX_VBR_quality_5", 5, true);
-  public static final SpeexEncoding SPEEX_VBR6 = new SpeexEncoding("SPEEX_VBR_quality_6", 6, true);
-  public static final SpeexEncoding SPEEX_VBR7 = new SpeexEncoding("SPEEX_VBR_quality_7", 7, true);
-  public static final SpeexEncoding SPEEX_VBR8 = new SpeexEncoding("SPEEX_VBR_quality_8", 8, true);
-  public static final SpeexEncoding SPEEX_VBR9 = new SpeexEncoding("SPEEX_VBR_quality_9", 9, true);
-  public static final SpeexEncoding SPEEX_VBR10 = new SpeexEncoding("SPEEX_VBR_quality_10", 10, true);
+  /** Specifies constant bitrate, quality 0, Speex encoding. */
+  public static final SpeexEncoding SPEEX_Q0 =
+    new SpeexEncoding("SPEEX_quality_0", 0, false);
+  /** Specifies constant bitrate, quality 1, Speex encoding. */
+  public static final SpeexEncoding SPEEX_Q1 =
+    new SpeexEncoding("SPEEX_quality_1", 1, false);
+  /** Specifies constant bitrate, quality 2, Speex encoding. */
+  public static final SpeexEncoding SPEEX_Q2 =
+    new SpeexEncoding("SPEEX_quality_2", 2, false);
+  /** Specifies constant bitrate, quality 3, Speex encoding. */
+  public static final SpeexEncoding SPEEX_Q3 =
+    new SpeexEncoding("SPEEX_quality_3", 3, false);
+  /** Specifies constant bitrate, quality 4, Speex encoding. */
+  public static final SpeexEncoding SPEEX_Q4 =
+    new SpeexEncoding("SPEEX_quality_4", 4, false);
+  /** Specifies constant bitrate, quality 5, Speex encoding. */
+  public static final SpeexEncoding SPEEX_Q5 =
+    new SpeexEncoding("SPEEX_quality_5", 5, false);
+  /** Specifies constant bitrate, quality 6, Speex encoding. */
+  public static final SpeexEncoding SPEEX_Q6 =
+    new SpeexEncoding("SPEEX_quality_6", 6, false);
+  /** Specifies constant bitrate, quality 7, Speex encoding. */
+  public static final SpeexEncoding SPEEX_Q7 =
+    new SpeexEncoding("SPEEX_quality_7", 7, false);
+  /** Specifies constant bitrate, quality 8, Speex encoding. */
+  public static final SpeexEncoding SPEEX_Q8 =
+    new SpeexEncoding("SPEEX_quality_8", 8, false);
+  /** Specifies constant bitrate, quality 9, Speex encoding. */
+  public static final SpeexEncoding SPEEX_Q9 =
+    new SpeexEncoding("SPEEX_quality_9", 9, false);
+  /** Specifies constant bitrate, quality 10, Speex encoding. */
+  public static final SpeexEncoding SPEEX_Q10 =
+    new SpeexEncoding("SPEEX_quality_10", 10, false);
+  /** Specifies variable bitrate, quality 0, Speex encoding. */
+  public static final SpeexEncoding SPEEX_VBR0 =
+    new SpeexEncoding("SPEEX_VBR_quality_0", 0, true);
+  /** Specifies variable bitrate, quality 1, Speex encoding. */
+  public static final SpeexEncoding SPEEX_VBR1 =
+    new SpeexEncoding("SPEEX_VBR_quality_1", 1, true);
+  /** Specifies variable bitrate, quality 2, Speex encoding. */
+  public static final SpeexEncoding SPEEX_VBR2 =
+    new SpeexEncoding("SPEEX_VBR_quality_2", 2, true);
+  /** Specifies variable bitrate, quality 3, Speex encoding. */
+  public static final SpeexEncoding SPEEX_VBR3 =
+    new SpeexEncoding("SPEEX_VBR_quality_3", 3, true);
+  /** Specifies variable bitrate, quality 4, Speex encoding. */
+  public static final SpeexEncoding SPEEX_VBR4 =
+    new SpeexEncoding("SPEEX_VBR_quality_4", 4, true);
+  /** Specifies variable bitrate, quality 5, Speex encoding. */
+  public static final SpeexEncoding SPEEX_VBR5 =
+    new SpeexEncoding("SPEEX_VBR_quality_5", 5, true);
+  /** Specifies variable bitrate, quality 6, Speex encoding. */
+  public static final SpeexEncoding SPEEX_VBR6 =
+    new SpeexEncoding("SPEEX_VBR_quality_6", 6, true);
+  /** Specifies variable bitrate, quality 7, Speex encoding. */
+  public static final SpeexEncoding SPEEX_VBR7 =
+    new SpeexEncoding("SPEEX_VBR_quality_7", 7, true);
+  /** Specifies variable bitrate, quality 8, Speex encoding. */
+  public static final SpeexEncoding SPEEX_VBR8 =
+    new SpeexEncoding("SPEEX_VBR_quality_8", 8, true);
+  /** Specifies variable bitrate, quality 9, Speex encoding. */
+  public static final SpeexEncoding SPEEX_VBR9 =
+    new SpeexEncoding("SPEEX_VBR_quality_9", 9, true);
+  /** Specifies variable bitrate, quality 10, Speex encoding. */
+  public static final SpeexEncoding SPEEX_VBR10 =
+    new SpeexEncoding("SPEEX_VBR_quality_10", 10, true);
 
   /** Default quality setting for the Speex encoding. */
   public static final int DEFAULT_QUALITY = 3;
