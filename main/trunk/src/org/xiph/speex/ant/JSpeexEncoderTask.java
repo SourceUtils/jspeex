67ffaf1c173ea1f5806ce04a549da04f59e850ff
mgimpel <mgimpel@677e43ea-1318-0410-b7e8-8a0e8f74a223>
2004-06-09 12:15:08 +0000

    First attempt at getting an ant task working

---
+++ main/trunk/src/org/xiph/speex/ant/JSpeexEncoderTask.java
@@ -0,0 +1,60 @@
+/******************************************************************************
+ *                                                                            *
+ * Copyright (c) 1999-2004 Wimba S.A., All Rights Reserved.                   *
+ *                                                                            *
+ * COPYRIGHT:                                                                 *
+ *      This software is the property of Wimba S.A.                           *
+ *      This software is redistributed under the Xiph.org variant of          *
+ *      the BSD license.                                                      *
+ *      Redistribution and use in source and binary forms, with or without    *
+ *      modification, are permitted provided that the following conditions    *
+ *      are met:                                                              *
+ *      - Redistributions of source code must retain the above copyright      *
+ *      notice, this list of conditions and the following disclaimer.         *
+ *      - Redistributions in binary form must reproduce the above copyright   *
+ *      notice, this list of conditions and the following disclaimer in the   *
+ *      documentation and/or other materials provided with the distribution.  *
+ *      - Neither the name of Wimba, the Xiph.org Foundation nor the names of *
+ *      its contributors may be used to endorse or promote products derived   *
+ *      from this software without specific prior written permission.         *
+ *                                                                            *
+ * WARRANTIES:                                                                *
+ *      This software is made available by the authors in the hope            *
+ *      that it will be useful, but without any warranty.                     *
+ *      Wimba S.A. is not liable for any consequence related to the           *
+ *      use of the provided software.                                         *
+ *                                                                            *
+ * Class: JSpeexEncoderTask.java                                              *
+ *                                                                            *
+ * Author: Marc GIMPEL                                                        *
+ *                                                                            *
+ * Date: Jun 4, 2004                                                          *
+ *                                                                            *
+ ******************************************************************************/
+
+/* $Id$ */
+
+package org.xiph.speex.ant;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+/**
+ * Ant <code>Task</code> to Encode an audio file from PCM Wave to Speex.
+ * 
+ * @author Marc Gimpel, Wimba S.A. (marc.gimpel@wimba.com)
+ * @version $Revision$
+ */
+public class JSpeexEncoderTask
+ extends Task
+{
+  /**
+   * The method executing the task.
+   * @throws BuildException
+   */
+  public void execute()
+    throws BuildException
+  {
+    
+  }
+}
